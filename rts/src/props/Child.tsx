e51f4b7852c04cd98cdab3288ef49d3a82c0a34b
NikMa <nikitama1hr@gmail.com>
2021-06-30 13:43:14 +0300

    added any type func (React.FC)

--- rts/src/props/Child.tsx
+++ rts/src/props/Child.tsx
@@ -6,3 +6,7 @@ interface  ChildProps {
 export const Child = ({color}: ChildProps) => {
     return <div>{color}</div>
 }
+
+export const ChildAsFC: React.FC<ChildProps> = ({color}) => {
+    return <div>{color}</div>
+}
