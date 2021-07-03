5a79b7f5abce46e8d29ff34327cce44403c62959
NikMa <nikitama1hr@gmail.com>
2021-07-03 21:40:57 +0300

    added some type ref el

---
+++ rts/src/refs/UserSearch.tsx
@@ -0,0 +1,32 @@
+import React, {useState, useRef} from 'react';
+
+const users = [
+    {name: 'Nik', age: 18},
+    {name: 'Jane', age: 19},
+    {name: 'Polly', age: 20},
+]
+
+
+const UserSearch: React.FC = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    const [name, setName] = useState('');
+    const [user, setUser] = useState<{ name: string, age: number } | undefined>();
+    const onClick = () => {
+        const foundUser = users.find((user) => {
+            return user.name === name
+        })
+        setUser(foundUser)
+    }
+
+    return (
+        <>
+            <h1>User Search</h1>
+            <input ref={inputRef} type="text" value={name} onChange={e => setName(e.target.value)}/>
+            <button onClick={onClick}>Find User</button>
+            {user && user.name},
+            {user && user.age}
+        </>
+    )
+};
+
+export default UserSearch;
