8492911b6bf11bed3e1f59796ac7f7bdf1455577
NikMa <nikitama1hr@gmail.com>
2021-07-03 21:46:54 +0300

    added some type on useEffect (check types)

--- rts/src/refs/UserSearch.tsx
+++ rts/src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 const users = [
     {name: 'Nik', age: 18},
@@ -11,6 +11,12 @@ const UserSearch: React.FC = () => {
     const inputRef = useRef<HTMLInputElement | null>(null);
     const [name, setName] = useState('');
     const [user, setUser] = useState<{ name: string, age: number } | undefined>();
+
+    useEffect(() => {
+      inputRef.current?.focus()
+    }, []);
+
+
     const onClick = () => {
         const foundUser = users.find((user) => {
             return user.name === name
