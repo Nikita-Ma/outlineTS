c45df91fd6ba1ce0cbb1d358c789bcf7af514f77
Nik <nikitama1hr@gmail.com>
2023-04-03 20:39:41 +0300

    repeat pure typescript

---
+++ boundler/src/message.ts
@@ -0,0 +1,121 @@
+{
+    enum StatusCodes {
+        NotFount = 404,
+        ServerError = 500,
+    }
+}
+
+
+{
+    let a: number = 1;
+    let b: bigint = 1n;
+    let c: string = 'String';
+    let d: symbol = Symbol();
+    let boo: boolean = true;
+//and null undefined object
+}
+
+{
+    let o: object = []
+
+    function foo(arr: string[]) {
+
+    }
+
+    foo(['2131'])
+}
+
+
+{
+    class Bla {
+        bar: string = '1'
+
+        foo(): number {
+            return 1
+        }
+    }
+
+    function getBla(a: Bla['constructor']) {
+
+
+    }
+
+    getBla(Bla)
+}
+
+{
+    let a: object = [];
+
+    function f(a: string | number | string[]) {
+        if (typeof a === 'number') {
+            a.toFixed()
+        }
+    }
+}
+
+{
+    let a: object = [];
+
+    class Foo {
+        foo: string = '12'
+    }
+
+    class Bar {
+        bar: number = 111
+    }
+
+    function foo(a: Foo & Bar) {
+        a.bar;
+        a.foo;
+    }
+
+    foo({...new Foo(), ...new Bar()})
+}
+
+{
+    function foo(a: 'true') {
+
+    }
+
+    foo('true')
+
+
+    function insertAdjacentHTML(str: string, append: 'beforebegin' | 'afterbegin') {
+
+    }
+
+    insertAdjacentHTML('anyString', 'beforebegin')
+}
+
+{
+    const a: unique symbol = Symbol('foo')
+}
+
+
+// Alias type
+{
+    type mode = `${'before' | 'after'}${'begin' | 'end'}`
+    type elType = string
+
+    function example(el: elType, html: mode) {
+        // ...code
+    }
+}
+
+{
+    interface Options {
+    cap?: boolean;
+    q?: boolean;
+    w?: string;
+    }
+
+    type OptionsType = {
+        cap?: boolean,
+        q?: boolean,
+        w?: string,
+    }
+
+    function f1(a?: Options) {
+
+    }
+}
